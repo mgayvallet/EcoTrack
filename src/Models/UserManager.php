628fb25e71c9e8b495da13f87eac63101e6dce05
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2026-06-15 10:22:57 +0200

    refactor: uncomment database connection code in UserManager

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -7,13 +7,11 @@ use MVC\Models\User;
 class UserManager
 {
 
-    // private $bdd;
+    private $bdd;
 
-    // public function __construct()
-    // {
-    //     $this->bdd = new \PDO('mysql:host=' . HOST . ';dbname=' . DATABASE . ';charset=utf8;', USER, PASSWORD);
-    //     $this->bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    // }
-
-
+    public function __construct()
+    {
+        $this->bdd = new \PDO('mysql:host=' . HOST . ';dbname=' . DATABASE . ';charset=utf8;', USER, PASSWORD);
+        $this->bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
 }
